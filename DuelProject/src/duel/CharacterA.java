e0e2b07d89e3b5a4de93a9324a06c00f06b579eb
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-09-14 11:40:47 -0400

    Added Classes

---
+++ DuelProject/src/duel/CharacterA.java
@@ -0,0 +1,35 @@
+package duel;
+
+public class CharacterA implements Dueler {
+
+	@Override
+	public void taunt() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStartingHP(int hp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getHP() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
